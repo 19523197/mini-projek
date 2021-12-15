fade9d8d77c036857fac5ec0b0128d7f38aaed33
Nabil Muhammad Firdaus <nabil.firdaus@uii.ac.id>
2021-12-15 17:11:29 +0700

    chore: Install unzip as required by composer

--- docker/setup.sh
+++ docker/setup.sh
@@ -2,12 +2,11 @@
 
 if [ "$IS_LOCAL" = "true" ]; then
     set -eux; \
-#    apt-get update; \
-#    apt-get install -y --no-install-recommends \
-#        iputils-ping \
-#        dnsutils \
-#    ; \
-#    rm -rf /var/lib/apt/lists/*; \
+    apt-get update; \
+    apt-get install -y --no-install-recommends \
+        unzip \
+    ; \
+    rm -rf /var/lib/apt/lists/*; \
     curl -sS https://getcomposer.org/installer | php; \
     mv composer.phar /usr/local/bin/; \
     ln -s /usr/local/bin/composer.phar /usr/local/bin/composer
